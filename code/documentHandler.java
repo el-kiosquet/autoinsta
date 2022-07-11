7dba9ebb15a41ff14bdad585b3cb71cbb3ed0639
elKiosquet <example@example.com>
2022-07-12 01:01:14 +0200

    Added isEmpty method to documentHandler
    New method is available and will return true if the file is empty, false otherwise.
    
    Also instaloaderHandler was improved and will show a message if no username was configured before.

--- code/documentHandler.java
+++ code/documentHandler.java
@@ -18,6 +18,14 @@ public class documentHandler {
         }
     }
 
+    /**
+     * Checks if the file is empty
+     * @return true if empty, false if it contains something.
+     */
+    public boolean isEmpty() {
+        return !readDocument.hasNext();
+    }
+
     /** Prints the hole document on the terminal */
     public void print() {
         while (readDocument.hasNext()) System.out.println(readDocument.nextLine());
--- code/instaloaderHandler.java
+++ code/instaloaderHandler.java
@@ -2,7 +2,11 @@ package code;
 
 public class instaloaderHandler {
     public static void downloaderUser() {
-        documentHandler downloaderUser = new documentHandler("code\\instaloaderHandler.java");
-        downloaderUser.print();
+        documentHandler downloaderUser = new documentHandler("config\\instaloader\\downloaderUsername.txt");
+        if (downloaderUser.isEmpty()) {
+            System.out.println("Still not any username configured!");
+        } else {
+            downloaderUser.print();
+        }
     }
 }
